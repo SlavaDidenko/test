4baf1dfcc6c8066d887f2b15533f5ebf3f91ca76
Andrew Datsenko <datsenko.andrew@gmail.com>
2023-04-05 01:39:49 +0300

    added/fixes

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,8 +4,6 @@ const map = [1, 2, 3].map((item) => {
   return item;
 });
 
-console.log(text, map, "1");
-
 function add(x, y) {
   // --->..return x + y;
 
@@ -19,7 +17,8 @@ function main() {
   var x = 5,
     y = 7;
 
-  console.log(x, y);
+  return x + y;
 }
 
-console.log(add(), main());
+add(text, map);
+main();
