161e4b6e3cdbc6f412a62afb1f8ae0af43c45ec1
Andrew Datsenko <datsenko.andrew@gmail.com>
2023-04-05 01:10:23 +0300

    check_10

--- resources/js/index.js
+++ resources/js/index.js
@@ -5,3 +5,21 @@ const map = [1, 2, 3].map((item) => {
 });
 
 console.log(text, map, "1");
+
+function add(x, y) {
+  // --->..return x + y;
+
+  return x + y;
+}
+
+function main() {
+  // --->var x = 5,
+  // --->....y = 7;
+
+  var x = 5,
+    y = 7;
+
+  console.log(x, y);
+}
+
+console.log(add(), main());
